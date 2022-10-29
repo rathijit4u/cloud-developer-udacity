58e8efc5e7b79b9f506f346bff093d4e27d37770
rathijit4u <rathijit.sarkar@gmail.com>
2022-10-29 16:41:57 +0530

    Build and Deploy - Success

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,16 +1,26 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "udagramrjdev",
+    "password": "udagramrjdev",
+    "database": "udagramrjdev",
+    "host": "udagramrjdev.ckgwsvgwewyn.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "us-east-2",
+    "aws_region": "us-east-1",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": "udagramrjdev"
+  },
+  "postgres": {
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DATABASE,
+    "host": process.env.POSTGRES_HOST,
+    "dialect": "postgres",
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
   "jwt": {
-    "secret": " "
+    "secret": process.env.JWT_SECRET
   },
   "prod": {
     "username": "",
--- course-02/exercises/udacity-c2-restapi/src/server.ts
+++ course-02/exercises/udacity-c2-restapi/src/server.ts
@@ -8,8 +8,13 @@ import bodyParser from 'body-parser';
 import { V0MODELS } from './controllers/v0/model.index';
 
 (async () => {
-  await sequelize.addModels(V0MODELS);
-  await sequelize.sync();
+  try {
+    await sequelize.addModels(V0MODELS);
+    await sequelize.sync();
+  } catch (error) {
+    console.log(error);
+  }
+
 
   const app = express();
   const port = process.env.PORT || 8080; // default port to listen
