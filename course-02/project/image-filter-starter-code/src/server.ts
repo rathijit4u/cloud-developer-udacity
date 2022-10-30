eb8149e1e94568f598bf6546c201e6772828bebe
rathijit4u <rathijit.sarkar@gmail.com>
2022-10-30 12:16:07 +0530

    finished project code

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
 (async () => {
 
@@ -9,7 +9,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -30,17 +30,36 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+  app.get("/filteredimage", async (req, res) => {
+    const imgUrl = req.query.image_url;
+    if (!imgUrl) {
+      res.status(404).send({ message: "Query parameter 'image_url' is missing." });
+    }
+
+    const filteredImg = await filterImageFromURL(imgUrl);
+
+    res.sendFile(filteredImg, (err) => {
+      if (err) {
+        console.log('Error sending image file. Details - ', err.message);
+      } else {
+        var filesArry: Array<string> = [filteredImg];
+        deleteLocalFiles(filesArry);
+      }
+    });
+  });
+
+
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get("/", async (req, res) => {
     res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  });
+
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
